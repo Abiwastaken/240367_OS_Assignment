db2c7072dcf86e0b5f8fe3a70b7dc42b315205df
Abiwastaken <abi.mukhiya@gmail.com>
2026-07-21 19:59:32 +0545

    cmake config setup file done

---
+++ build/CMakeFiles/OSSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSSimulator.dir/src/main.cpp.o"
+  "CMakeFiles/OSSimulator.dir/src/main.cpp.o.d"
+  "OSSimulator"
+  "OSSimulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OSSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OSSimulator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for OSSimulator.
+# This may be replaced when dependencies are built.
